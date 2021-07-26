b6e35cb4a662e46af28e4ed06bed692e5c3a97b8
EthanMathis <ethan.mathis@outlook.com>
2021-07-26 15:38:35 -0500

    forgot to save the repo. lul

--- Randomizer/Repositories/CharacterRepository.cs
+++ Randomizer/Repositories/CharacterRepository.cs
@@ -95,6 +95,121 @@ namespace Randomizer.Repositories
             }
         }
 
+        public Character RandomCharacter()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT Alignment.Id AS AlignmentId, Alignment.Name AS AlignmentName
+	                                           FROM Alignment 
+	                                           WHERE Alignment.Id = FLOOR(RAND()*(7)+1);
+
+                                        SELECT Race.Id AS RaceId, Race.Name AS RaceName
+	                                           FROM Race 
+	                                           WHERE Race.Id = FLOOR(RAND()*(14)+1);
+
+                                        SELECT Gender.Id AS GenderId, Gender.Name AS GenderName
+	                                           FROM Gender 
+	                                           WHERE Gender.Id = FLOOR(RAND()*(2)+1);
+
+                                        SELECT AppearanceFeature.Id AS AppearanceFeatureId, AppearanceFeature.Description AS AppearanceFeatureDescription
+	                                           FROM AppearanceFeature 
+	                                           WHERE AppearanceFeature.Id = FLOOR(RAND()*(18)+1);
+
+                                        SELECT InteractionTrait.Id AS InteractionTraitId, InteractionTrait.Name AS InteractionTraitName
+	                                           FROM InteractionTrait 
+	                                           WHERE InteractionTrait.Id = FLOOR(RAND()*(13)+1);
+
+                                        SELECT Mannerism.Id AS MannerismId, Mannerism.Description AS MannerismDescription
+	                                           FROM Mannerism 
+	                                           WHERE Mannerism.Id = FLOOR(RAND()*(17)+1);
+
+                                        SELECT Talent.Id AS TalentId, Talent.Description AS TalentDescription
+	                                           FROM Talent 
+	                                           WHERE Talent.Id = FLOOR(RAND()*(17)+1);";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var character = new Character();
+
+                    while (reader.Read())
+                    {
+                        character.AlignmentId = DbUtils.GetInt(reader, "AlignmentId");
+                        character.Alignment = new Alignment()
+                        {
+                            Id = DbUtils.GetInt(reader, "AlignmentId"),
+                            Name = DbUtils.GetString(reader, "AlignmentName")
+                        };
+                    }
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        character.RaceId = DbUtils.GetInt(reader, "RaceId");
+                        character.Race = new Race()
+                        {
+                            Id = DbUtils.GetInt(reader, "RaceId"),
+                            Name = DbUtils.GetString(reader, "RaceName")
+                        };
+                    }
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        character.GenderId = DbUtils.GetInt(reader, "GenderId");
+                        character.Gender = new Gender()
+                        {
+                            Id = DbUtils.GetInt(reader, "GenderId"),
+                            Name = DbUtils.GetString(reader, "GenderName")
+                        };
+                    }
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        character.AppearanceFeatureId = DbUtils.GetInt(reader, "AppearanceFeatureId");
+                        character.AppearanceFeature = new AppearanceFeature()
+                        {
+                            Id = DbUtils.GetInt(reader, "AppearanceFeatureId"),
+                            Description = DbUtils.GetString(reader, "AppearanceFeatureDescription")
+                        };
+                    }
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        character.InteractionTraitId = DbUtils.GetInt(reader, "InteractionTraitId");
+                        character.InteractionTrait = new InteractionTrait()
+                        {
+                            Id = DbUtils.GetInt(reader, "InteractionTraitId"),
+                            Name = DbUtils.GetString(reader, "InteractionTraitName")
+                        };
+                    }
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        character.MannerismId = DbUtils.GetInt(reader, "MannerismId");
+                        character.Mannerism = new Mannerism()
+                        {
+                            Id = DbUtils.GetInt(reader, "MannerismId"),
+                            Description = DbUtils.GetString(reader, "MannerismDescription")
+                        };
+                    }
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        character.TalentId = DbUtils.GetInt(reader, "TalentId");
+                        character.Talent = new Talent()
+                        {
+                            Id = DbUtils.GetInt(reader, "TalentId"),
+                            Description = DbUtils.GetString(reader, "TalentDescription")
+                        };
+                    }
+                    reader.Close();
+                    return character;
+                }
+            }
+        }
+
         public void AddCharacter(Character character)
         {
             using (var conn = Connection)
